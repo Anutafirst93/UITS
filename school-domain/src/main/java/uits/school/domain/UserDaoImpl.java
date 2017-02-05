b6d6c3dba9f1f6453c7a3ef40f845fd28b107c60
stan-o <orlov.stanislav.e@gmail.com>
2017-02-05 13:40:22 +0200

    groups template

--- school-domain/src/main/java/uits/school/domain/UserDaoImpl.java
+++ school-domain/src/main/java/uits/school/domain/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package uits.school.domain;
 
+import java.util.List;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,5 +13,12 @@ public class UserDaoImpl extends AbstractDao<User, Integer> implements UserDao {
     public User byUsername(String name) {
         return (User) getCurrentSession().createCriteria(User.class)
                 .add(Restrictions.eq("username", name)).uniqueResult();
+    }
+
+    @Override
+    public List<User> ListStudentsByGroupId(Integer id) {
+        return getCurrentSession().createSQLQuery("SELECT * FROM `user` where id in "
+                + "(select id_student from students_list where id_group = :id)").addEntity(User.class)
+                .setParameter("id", id).list();
     }
 }
--- school-web/src/main/java/uits/school/web/controller/admin/GroupsController.java
+++ school-web/src/main/java/uits/school/web/controller/admin/GroupsController.java
@@ -1,19 +1,27 @@
 package uits.school.web.controller.admin;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import uits.school.domain.GroupTime;
 import uits.school.domain.Groups;
 import uits.school.domain.GroupsDao;
+import uits.school.domain.User;
+import uits.school.domain.UserDao;
 
 @Controller
 public class GroupsController {
     
     @Autowired
     private GroupsDao groupsDao;
+    
+    @Autowired
+    private UserDao userDao;
     
     @Transactional
     @RequestMapping(value = "/admin/groups")
@@ -24,9 +32,88 @@ public class GroupsController {
         List<Groups> groupsList = groupsDao.list();
         
         ModelAndView mv = new ModelAndView("admin/groups/home");
-        mv.addObject("groups", groupsList);
+        List<GroupsModel> model = new ArrayList();
+        for(Groups g : groupsList){
+            model.add(new GroupsModel(g));
+        }
+        mv.addObject("groups", model);
         
         return mv;
+    }
+    
+    @Transactional
+    @RequestMapping(value = "/admin/groups/{id}")
+    public ModelAndView viewSelectedGroup(@PathVariable("id") Integer id){
+        //get groups list
+        //for each group get teacher
+        
+        Groups group = groupsDao.byId(id);
+        
+        List<User> students = userDao.ListStudentsByGroupId(id);
+        
+//        List<Groups> groupsList = groupsDao.list();
+//        
+        ModelAndView mv = new ModelAndView("admin/groups/view");
+//        List<GroupsModel> model = new ArrayList();
+//        for(Groups g : groupsList){
+//            model.add(new GroupsModel(g));
+//        }
+        mv.addObject("group", new GroupsModel(group));
+        mv.addObject("students", students);
+        
+        return mv;
+    }
+    
+    public class GroupsModel{
+        private Integer id;
+        private String name;
+        private String teacherName;
+        private List<String> days;
+
+        public GroupsModel(Groups group) {
+            days = new ArrayList<>();
+            this.id = group.getId();
+            this.name = group.getName();
+            teacherName = "test";
+            for(GroupTime t : group.getGroupTimeList()){
+                days.add(t.getDayOfWeek() + " " + t.getStartTime() );
+            }
+        }
+        
+        
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getTeacherName() {
+            return teacherName;
+        }
+
+        public void setTeacherName(String teacherName) {
+            this.teacherName = teacherName;
+        }
+
+        public List<String> getDays() {
+            return days;
+        }
+
+        public void setDays(List<String> days) {
+            this.days = days;
+        }
+        
     }
     
 }
